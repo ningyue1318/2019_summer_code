f73c9e86a007efb47be40c827f9bd6ea6dfde18c
syn <1277323460@qq.com>
2019-08-10 08:39:31 +0800

    complete the chapter07

--- python编程高手之路/练习/chapter07.py
+++ python编程高手之路/练习/chapter07.py
@@ -1,3 +1,7 @@
+from functools import wraps
+from datetime import datetime
+import time
+
 def auth():
     print('登录。。。。。')
 
@@ -18,16 +22,123 @@ def pay():
     print('支付。。。。。。')
 
 
-func_dict = {
-    '1': auth,
-    '2': register,
-    '3': check,
-    '4': transfer,
-    '5': pay
-}
+def small_example():
+    func_dict = {
+        '1': auth,
+        '2': register,
+        '3': check,
+        '4': transfer,
+        '5': pay
+    }
 
-choice = input('>>:').strip()
-if choice in func_dict:
-    func_dict[choice]()
-else:
-    print('非法操作')
+    choice = input('>>:').strip()
+    if choice in func_dict:
+        func_dict[choice]()
+    else:
+        print('非法操作')
+
+
+"""
+-----------------------------------------------------------------------------
+第七章作业
+"""
+
+
+"""
+第一个作业
+"""
+user = {'username': ''
+        }
+
+
+def check_login_decorator(func):
+
+    @wraps(func)
+    def check_login(*args):
+        if user['username'] == '':
+            login()
+        func(*args)
+    return check_login
+
+
+def login():
+    username = input('请输入用户名:\n')
+    password = input('请输入密码:\n')
+    if username == 'bob' and password == '123':
+        user['username'] = username
+        print('登录成功')
+
+
+@check_login_decorator
+def shopping():
+    print('正在购物中')
+
+
+shopping()
+
+
+"""
+第二个作业
+"""
+user = {'login_time': datetime.today()
+        }
+
+
+def check_login_decorator(func):
+
+    @wraps(func)
+    def check_login(*args):
+        time.sleep(6)
+        if (user['login_time'] - datetime.today()).seconds > 5:
+            login()
+        func(*args)
+    return check_login
+
+
+def login():
+    username = input('请输入用户名:\n')
+    password = input('请输入密码:\n')
+    if username == 'bob' and password == '123':
+        user['login_time'] = datetime.today()
+        print('登录成功')
+
+
+@check_login_decorator
+def shopping():
+    print('正在购物中')
+
+
+shopping()
+
+
+"""
+第三个作业
+"""
+
+
+def record_time_decorator(filename):
+    def record_time_(func):
+        @wraps(func)
+        def record_the_time(*args):
+            record_item = func.__name__ +" "+str(datetime.today())+"\n"
+            with open(filename, mode='a', encoding='utf-8') as f:
+                f.write(record_item)
+            print('记录完成')
+            func(*args)
+
+        return record_the_time
+    return record_time_
+
+
+@record_time_decorator(filename='log.txt')
+def function_test():
+    print('装饰器函数测试')
+
+
+@record_time_decorator(filename='log.txt')
+def add_number(a, b):
+    print(a+b)
+
+
+add_number(100, 100)
+function_test()
